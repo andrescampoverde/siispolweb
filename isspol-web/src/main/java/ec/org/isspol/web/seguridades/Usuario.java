d9affc88204a34c1a0ee88a9ed7c77c556cfe0e4
greenintegrationdesarrollo <afcampoverderengel@gmail.com>
2017-03-30 16:51:48 -0500

    metodos usuario cargos y estado

--- isspol-web/src/main/java/ec/org/isspol/web/seguridades/Usuario.java
+++ isspol-web/src/main/java/ec/org/isspol/web/seguridades/Usuario.java
@@ -1,5 +1,6 @@
 package ec.org.isspol.web.seguridades;
 
+import ec.org.isspol.persistence.entities.security.EstadoUsuario;
 import ec.org.isspol.persistence.entities.security.Horario;
 import ec.org.isspol.persistence.entities.security.OficinaSucursal;
 import ec.org.isspol.route.client.UsuarioServiceClient;
@@ -8,6 +9,7 @@ import ec.org.isspol.route.client.context.ApplicartionContext;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by santiago.yacelga on 29/03/2017.
@@ -20,5 +22,21 @@ public class Usuario implements Serializable {
         return lstHorarios;
     };
 
+    public Map<String, Object> obtenerCargos(){
+        UsuarioServiceClient cargos = (UsuarioServiceClient) ApplicartionContext.getBean("usuarioServiceClient");
+        Map<String, Object> mapCargos = cargos.getAllCargo();
+        return mapCargos;
+    }
 
+    public List<EstadoUsuario> obtenerEstados (){
+        UsuarioServiceClient client = (UsuarioServiceClient) ApplicartionContext.getBean("usuarioServiceClient");
+        List<EstadoUsuario> lstEstadosUsuario = client.getAllEstadoUsuario();
+        return lstEstadosUsuario;
+    }
+
+    public List<OficinaSucursal> obtenerOficinas(){
+        UsuarioServiceClient client = (UsuarioServiceClient) ApplicartionContext.getBean("usuarioServiceClient");
+        List<OficinaSucursal> lstOficinasSucursal = client.getAllOficinaSucursal();
+        return lstOficinasSucursal;
+    }
 }
